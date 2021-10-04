5dafd1298aef551da8e8bc29bb524649dbda453a
kindmemento <kindmemento@gmail.com>
2021-10-04 18:30:07 +0300

    webpack dependencies updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const path = require('path');
+const dotenv = require('dotenv-webpack')
 
 module.exports = {
+  mode: 'development',
   entry: './src/main.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devtool: 'inline-source-map',
+  plugins: [
+    new dotenv
+  ]
 };
